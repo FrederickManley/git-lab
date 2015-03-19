f86a9b03bc2e7db33b6c798058b269bb89f4c1a6
Fred Manley <fmanley@northpointdigital.com>
2015-03-19 13:14:33 -0400

    Third Thursday Lab

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -1,7 +1,7 @@
 public class Conflicts
 {
 	public string ConflictOne { get; set;}
-	public string ConflictTwo { get; set;}
+	public int ConflictTwo { get; set;}
 	public string ConflictThree { get; set; }
 	public string NotAConflict { get; set; }
 }
