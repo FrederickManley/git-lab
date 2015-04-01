4db3d17f1b93915ca06a6cd8c14818ae293b7f45
Fred Manley <fmanley@northpointdigital.com>
2015-04-01 09:14:36 -0400

    merge feature and master

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -1,7 +1,7 @@
 public class Conflicts
 {
-	public int ConflictOne { get; set;}
-	public double ConflictTwo { get; set;}
+	public string ConflictOne { get; set;}
+	public int ConflictTwo { get; set;}
 	public int ConflictThree { get; set; }
 	public string NotAConflict { get; set; }
 }
