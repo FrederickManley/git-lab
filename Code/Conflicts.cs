c29d8ce42dffcc3cf8e7f853b818ae540614530f
Fred Manley <fmanley@northpointdigital.com>
2015-04-01 09:08:15 -0400

    feature-01

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -1,6 +1,6 @@
 public class Conflicts
 {
-	public string ConflictOne { get; set;}
+	public int ConflictOne { get; set;}
 	public int ConflictTwo { get; set;}
 	public int ConflictThree { get; set; }
 	public string NotAConflict { get; set; }
