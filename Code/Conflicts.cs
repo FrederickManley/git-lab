731454118d1735f21309c9c63d5f1cf82876f1e7
Fred Manley <fmanley@northpointdigital.com>
2015-04-06 09:05:45 -0400

    another conflict again

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -1,7 +1,13 @@
 public class Conflicts
 {
+<<<<<<< HEAD
 	public Int34 ConflictOne { get; set;}
 	public double ConflictTwo { get; set;}
 	public string ConflictThree { get; set; }
+=======
+	public int ConflictOne { get; set;}
+	public string ConflictTwo { get; set;}
+	public double ConflictThree { get; set; }
+>>>>>>> another conflict again
 	public string NotAConflict { get; set; }
 }
