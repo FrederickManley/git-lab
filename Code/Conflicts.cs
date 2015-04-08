104a7256eb051edaabcb3f4fbe5753c9048e6037
Fred Manley <fmanley@northpointdigital.com>
2015-04-08 13:55:09 -0400

    hotfix example

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -1,6 +1,6 @@
 public class Conflicts
 {
-	public int ConflictOne { get; set;}
+	public string ConflictOne { get; set;}
 	public double ConflictTwo { get; set;}
 	public int ConflictThree { get; set; }
 	public string NotAConflict { get; set; }
