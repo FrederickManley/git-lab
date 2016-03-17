9ee2faa0d161b53e397890bc95b2e29e9b7e14c3
Benji Fisher <benji.fisher@ey.com>
2016-03-17 13:39:31 -0400

    Comment madness!

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -3,8 +3,9 @@ public class Conflicts
 	public Int34 ConflictOne { get; set;}
 	public double ConflictTwo { get; set;}
 	public string ConflictThree { get; set; }
-	public int ConflictOne { get; set;}
+	public int ConflictOne { get; set;} # stupid comment
 	public string ConflictTwo { get; set;}
 	public double ConflictABC{ get; set; }
+## There are not enough comments in this code!
 	public string NotAConflict { get; set; }
 }
