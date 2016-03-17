2a6d5967ee6b7b4c06bef85349d89d23ab5f165f
Bhavin Desai <Bhavin.Desai1@ey.com>
2016-03-17 13:41:06 -0400

    Change made by Bhavin Desai

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -1,10 +1,9 @@
 public class Conflicts
 {
 	public Int34 ConflictOne { get; set;}
-	public double ConflictTwo { get; set;}
+	public string ConflictTwo { get; set;}
 	public string ConflictThree { get; set; }
 	public string ConflictOne { get; set;}
 	public string ConflictTwo { get; set;}
 	public double ConflictABC{ get; set; }
-	public string NotAConflict { get; set; }
 }
