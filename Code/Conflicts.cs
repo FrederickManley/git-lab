f925fa74f393521b8766b81fff3cfd613aa3d6b8
Frederick Manley <Frederick.Manley@us.na.ey.net>
2016-03-17 10:43:41 -0400

    A conflict to be merged

--- Code/Conflicts.cs
+++ Code/Conflicts.cs
@@ -3,7 +3,7 @@ public class Conflicts
 	public Int34 ConflictOne { get; set;}
 	public double ConflictTwo { get; set;}
 	public string ConflictThree { get; set; }
-	public double ConflictOne { get; set;}
+	public double MergeConflict { get; set;}
 	public string ConflictTwo { get; set;}
 	public double ConflictABC{ get; set; }
 	public string NotAConflict { get; set; }
